27e0199dc5f7126d3f90ebcf3b742e76a74ffd7e
takeru.hashimoto <hstake25@gmail.com>
2024-02-15 23:22:54 +0900

    それっぽく

--- 依存する、している/sample.go
+++ 依存する、している/sample.go
@@ -1,11 +1,29 @@
 package main
 
+import "fmt"
+
+type PCParts struct {
+	Name string
+}
+
+func (p PCParts) Exec() {
+	fmt.Printf("this parts is: %s\n", p.Name)
+}
+
 type PC struct {
 	PCParts PCParts
 }
 
-func (p PC) Start() string {
-	return "fanfanfan"
+func NewPC() *PC {
+	pCParts := PCParts{Name: "CPU"}
+
+	return &PC{
+		// PC は PCParts に依存している
+		PCParts: pCParts,
+	}
 }
 
-type PCParts struct{}
+func main() {
+	pc := NewPC()
+	pc.PCParts.Exec()
+}
